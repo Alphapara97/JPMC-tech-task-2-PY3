d0d0f9aa7e1c7850212c465619ec500f59d4e78a
ansuman sasmal <asasmal@syr.edu>
2022-06-26 22:52:42 -0400

    'Create_Patch_File'

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +23,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
+      // adding a boolean to create whether to show the graph -> (by Veeresh S)
     };
   }
 
@@ -29,18 +32,30 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    // show graph only if showgraph boolean is true -(by Veeresh S)
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data} />)
+      }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    //  instead of storing data when we click button we create function and it runs every 100 ms - (by Veeresh S)
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
@@ -70,5 +85,6 @@ class App extends Component<{}, IState> {
     )
   }
 }
+
 
 export default App;
